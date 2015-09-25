59c832ff798cccbb191e1738d0a51559e5942372
Charles Bittner-Rossmiller <charles.bittnerrossmiller@gmail.com>
2015-09-25 11:51:13 -0500

    Schema adjusted and 2 migrations done. User and jobs tables created

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925150938) do
+ActiveRecord::Schema.define(version: 20150925163227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "user"
+    t.string   "title"
+    t.string   "post_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",            null: false
@@ -25,5 +33,7 @@ ActiveRecord::Schema.define(version: 20150925150938) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+  add_foreign_key "jobs", "users"
 
 end
